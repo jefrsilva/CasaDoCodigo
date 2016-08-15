3611c79ba9f75ba5d17c44cf3dc5bb3c4aa0cfe3
jefrsilva <jefrsilva@gmail.com>
2016-08-15 15:52:14 -0300

    Exercício 1 - RecyclerView

---
+++ app/src/main/java/br/com/caelum/casadocodigo/adapter/LivroAdapter.java
@@ -0,0 +1,60 @@
+package br.com.caelum.casadocodigo.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.caelum.casadocodigo.R;
+import br.com.caelum.casadocodigo.modelo.Livro;
+
+public class LivroAdapter extends RecyclerView.Adapter {
+    private List<Livro> livros;
+
+    public LivroAdapter(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int tipoDeLayout = R.layout.item_livro_par;
+        if (viewType % 2 != 0) {
+            tipoDeLayout = R.layout.item_livro_impar;
+        }
+        View view = LayoutInflater.from(parent.getContext()).inflate(tipoDeLayout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        Livro livro = livros.get(position);
+        viewHolder.nome.setText(livro.getNome());
+    }
+
+    @Override
+    public int getItemCount() {
+        return livros.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2;
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView nome;
+        public final ImageView foto;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            nome = (TextView) view.findViewById(R.id.item_livro_nome);
+            foto = (ImageView) view.findViewById(R.id.item_livro_foto);
+        }
+    }
+}
